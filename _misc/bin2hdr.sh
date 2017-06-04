0ec4d4afccb4c82da8693ca1706c810044fcfe4f
Marcell Tarjan <tarjan.marcell@gmail.com>
2017-06-04 02:03:20 +0100

    bin2hdr.sh even cooler

--- _misc/bin2hdr.sh
+++ _misc/bin2hdr.sh
@@ -2,12 +2,12 @@
 
 # Binary to C static array header definition convereter
 
-[ $# -lt 1 ] && echo "Usage: `basename $0` <binary_to_convert> [array_name]" && echo "  If array_name is specified file will be saved as array_name.h (defaults to boot_bin)" && exit 1
-[ ! -e "$1" ] && echo "Error: file '$1' not found" && exit 1
+[ $# -lt 1 ] && echo "Usage: `basename $0` <binary_to_convert> [array_name]" && echo " If array_name is specified file will be saved as array_name.h (defaults to boot_bin)" && exit 1
+[ ! -e "$1" ] && echo "Error: file '$1' not found" && exit 2
 
 HDR="boot_bin"
 [ $# -gt 1 ] && HDR="$2"
-echo "$HDR" | grep -q '\W' && echo "Invalid array_name \"$HDR\"" && exit 2
+echo "$HDR" | grep -q '\W' && echo "Invalid array_name \"$HDR\"" && exit 3
 [ -e "$HDR.h" ] && mv -v "$HDR.h" "$HDR.h~"
 
 echo "const unsigned char $HDR[] = { \\" > "$HDR.h"

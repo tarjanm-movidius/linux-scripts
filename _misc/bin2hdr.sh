89baf312d2b62bb81e5f794bf5a18fe7cd2c22e2
Marcell Tarjan <tarjan.marcell@gmail.com>
2017-06-04 01:29:36 +0100

    bin2hdr.sh array name parameter

--- _misc/bin2hdr.sh
+++ _misc/bin2hdr.sh
@@ -2,13 +2,13 @@
 
 # Binary to C static array header definition convereter
 
-[ $# -lt 1 ] && echo "Usage: `basename $0` <binary_to_convert> [hex_header_name.h]" && exit 1
+[ $# -lt 1 ] && echo "Usage: `basename $0` <binary_to_convert> [array_name]" && echo "  If array_name is specified file will be saved as array_name.h (defaults to boot_bin)" && exit 1
 [ ! -e "$1" ] && echo "Error: file '$1' not found" && exit 1
 
-HDR="boot_bin.h"
+HDR="boot_bin"
 [ $# -gt 1 ] && HDR="$2"
-[ -e "$HDR" ] && mv "$HDR" "$HDR~"
+[ -e "$HDR.h" ] && mv -v "$HDR.h" "$HDR.h~"
 
-echo "const unsigned char boot_bin[] = { \\" > "$HDR"
-hexdump -v -e '16/1 " 0x%02X," " \\\n"' "$1" | sed 's/0x  , //g' | head -c -4 >> "$HDR"
-echo " };" >> "$HDR"
+echo "const unsigned char $HDR[] = { \\" > "$HDR.h"
+hexdump -v -e '16/1 " 0x%02X," " \\\n"' "$1" | sed 's/0x  , //g' | head -c -4 >> "$HDR.h"
+echo " };" >> "$HDR.h"

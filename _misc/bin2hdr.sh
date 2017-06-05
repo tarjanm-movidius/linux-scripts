e7db57fe639f32e121c89668deed76aa38c8d69b
Marcell Tarjan <tarjan.marcell@gmail.com>
2017-06-06 00:17:36 +0100

    Added sqlist

--- _misc/bin2hdr.sh
+++ _misc/bin2hdr.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
-# Binary to C static array header definition convereter
+# Binary to C static array header definition converter
 
-[ $# -lt 1 ] && echo "Usage: `basename $0` <binary_to_convert> [array_name]" && echo " If array_name is specified file will be saved as array_name.h (defaults to boot_bin)" && exit 1
+[ $# -lt 1 ] && echo "Usage: `basename $0` <binary_to_convert> [array_name]" && echo " If array_name is specified file will be saved as <array_name>.h (defaults to boot_bin)" && exit 1
 [ ! -e "$1" ] && echo "Error: file '$1' not found" && exit 2
 
 HDR="boot_bin"
 [ $# -gt 1 ] && HDR="$2"
-echo "$HDR" | grep -q '\W' && echo "Invalid array_name \"$HDR\"" && exit 3
+echo "$HDR" | grep -q '\W' && echo "Invalid array_name '$HDR'" && exit 3
 [ -e "$HDR.h" ] && mv -v "$HDR.h" "$HDR.h~"
 
 echo "const unsigned char $HDR[] = { \\" > "$HDR.h"

c1da042b45e3f9e130664459a1f361c77b4a8180
Marcell Tarjan <tarjan.marcell@gmail.com>
2017-08-07 18:11:04 +0100

    Updated configs to match system

---
+++ _misc/arch/etc/profile.d/proxy.sh
@@ -0,0 +1,6 @@
+export http_proxy="http://proxy.server.com:8080"
+export https_proxy="https://proxy.server.com:8443"
+export ftp_proxy="http://proxy.server.com:1080"
+export rsync_proxy=$http_proxy
+export no_proxy="localhost,127.0.0.1,archvm"
+#,.movidius.com

9c8e06701bac6cc896adef048ea9bee8d8ff9f58
Marcell Tarjan <tarjan.marcell@gmail.com>
2018-05-22 02:51:58 +0100

    ASUS new kernel & updates

--- _misc/tag.sh
+++ _misc/tag.sh
@@ -5,7 +5,7 @@ IFS='
 
 for i in `find . -name "*.mp3"`; do
 	if ! id3info "$i" | grep -q 'TIT\|TT2'; then
-		NAME="`basename $i | sed 's/.mp3//g' | sed 's/.* - //g'`"
+		NAME="`basename $i | sed 's/.* - \|.mp3\| lyrics\| official video\| video//gI'`"
 		id3tag -1 -s"$NAME" "$i"
 	fi
 done

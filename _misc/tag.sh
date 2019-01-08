e10857555588626f33ae2d84a88df3e74ec5bd2e
Marcell Tarjan <tarjan.marcell@gmail.com>
2019-01-08 03:30:00 +0000

    Kernel 4.14.63 + minor updates

--- _misc/tag.sh
+++ _misc/tag.sh
@@ -2,8 +2,12 @@
 
 IFS='
 '
-
-for i in `find . -name "*.mp3"`; do
+if [ -n "$1" ]; then
+	NAMES="$1"
+else
+	NAMES="`find . -name "*.mp3"`"
+fi
+for i in $NAMES; do
 	if ! id3info "$i" | grep -q 'TIT\|TT2'; then
 		NAME="`basename $i | sed 's/.* - \|.mp3\| lyrics\| official video\| video//gI'`"
 		id3tag -1 -s"$NAME" "$i"
